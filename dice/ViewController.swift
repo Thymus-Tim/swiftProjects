a7248dc4c441c50275da0acff2bf900689389e3f
Tim <timurdjakhangirov@gmail.com>
2022-12-13 17:13:56 +0500

    commit

--- dice/ViewController.swift
+++ dice/ViewController.swift
@@ -11,14 +11,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var logo: UIImageView!
     
+    @IBOutlet weak var label: UILabel!
     @IBOutlet weak var diceB: UIImageView!
-    
+    @IBOutlet var score: UILabel!
+    var result = 0
     @IBOutlet weak var diceA: UIImageView!
     
     @IBAction func butt(_ sender: UIButton) {
         let diceImages = [#imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
-        diceA.image = diceImages[Int.random(in: 0...5)]
-        diceB.image = diceImages[Int.random(in: 0...5)]
+        let a = Int.random(in: 0...5)
+        let b = Int.random(in: 0...5)
+        diceA.image = diceImages[a]
+        diceB.image = diceImages[b]
+        result = a + b + 2
+        score.text = "\(result)"
     }
     override func viewDidLoad() {
        
